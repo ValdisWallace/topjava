300f51bc7bc455573e3de2c7e442885ddf530c30
ValdisWallace <valdisvanwallace@gmail.com>
2019-07-27 18:09:35 +0800

    add controller

---
+++ src/main/java/ru/javawebinar/topjava/web/JpaMealController.java
@@ -0,0 +1,102 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
+@RequestMapping(value = "/meals")
+public class JpaMealController {
+    private static final Logger log = LoggerFactory.getLogger(JpaMealController.class);
+
+    @Autowired
+    private MealService service;
+
+    private int userId = SecurityUtil.authUserId();
+
+    @PostMapping(params = {"id", "dateTime", "description", "calories"})
+    public String save(Model model,
+                       @RequestParam(defaultValue = "0") int id,
+                       @RequestParam String dateTime,
+                       @RequestParam String description,
+                       @RequestParam int calories) {
+        Meal meal = new Meal(LocalDateTime.parse(dateTime), description, calories);
+
+        if (id != 0) {
+            assureIdConsistent(meal, id);
+            log.info("update {} for user {}", meal, userId);
+            service.update(meal, userId);
+        } else {
+            checkNew(meal);
+            log.info("create {} for user {}", meal, userId);
+            service.create(meal, userId);
+        }
+        return getMeals(model);
+    }
+
+    @GetMapping
+    public String getMeals(Model model) {
+        log.info("getAll for user {}", userId);
+        model.addAttribute("meals", MealsUtil.getWithExcess(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
+    }
+
+    @GetMapping(params = "delete")
+    public String delete(Model model, @RequestParam("delete") int id) {
+        log.info("delete meal {} for user {}", id, userId);
+        service.delete(id, userId);
+        getMeals(model);
+        return "meals";
+    }
+
+    @GetMapping(params = "create")
+    public String create(Model model) {
+        final Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        log.info("create {} for user {}", meal, userId);
+        model.addAttribute("meal", meal);
+        return "/mealForm";
+    }
+
+    @GetMapping(params = "update")
+    public String update(Model model, @RequestParam("update") int id) {
+        final Meal meal = service.get(id, userId);
+        log.info("update {} for user {}", meal, userId);
+        model.addAttribute("meal", meal);
+        return "/mealForm";
+    }
+
+    @GetMapping(params = {"startDate", "endDate", "startTime", "endTime"})
+    public String getFiltered(Model model,
+                              @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                              @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+                              @RequestParam LocalTime startTime,
+                              @RequestParam LocalTime endTime) {
+        model.addAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
+    }
+
+
+    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        log.info("getBetween dates({} - {}) time({} - {}) for user {}", startDate, endDate, startTime, endTime, userId);
+
+        List<Meal> mealsDateFiltered = service.getBetweenDates(startDate, endDate, userId);
+        return MealsUtil.getFilteredWithExcess(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
+    }
+}
