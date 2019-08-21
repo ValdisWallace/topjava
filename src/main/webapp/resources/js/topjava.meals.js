2bd5146a8f9437729f48b301f7e0bb8584d82bca
ValdisWallace <valdisvanwallace@gmail.com>
2019-08-21 20:11:40 +0700

    fix excess

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -56,7 +56,10 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-mealExcess", data.excess);
+            }
         }),
         updateTable: updateFilteredTable
     });
